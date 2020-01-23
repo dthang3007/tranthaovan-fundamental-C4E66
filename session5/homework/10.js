5024b233ee8745cbae5650a905bd245cf34d4b77
Thao Van <thaovannihong@gmail.com>
2020-01-23 17:46:51 +0700

    completed

---
+++ session5/homework/10.js
@@ -0,0 +1,12 @@
+let arr1 = prompt('x, y : ').split(',');
+let arr2 = prompt('x, y, width, height : ').split(',');
+
+for (let i = 0; i < arr1.length; i++) {
+    console.log(arr1[i]);
+}
+
+console.log('\n\n');
+for (let j = 0; j < arr2.length; j++) {
+    console.log(arr2[j]);
+}
+
